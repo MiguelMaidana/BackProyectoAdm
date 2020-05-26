b2b1c0f5b165488f8d59805b26821cab9d5c76f7
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-26 20:03:05 -0300

    Creo el type Usuario, el inputUsuario

--- db/resolvers.js
+++ db/resolvers.js
@@ -6,7 +6,15 @@
 const resolvers ={
     Query:{
         obtenerCurso:()=>"Algo"
+    },
+    Mutation :{
+        nuevoUsuario:(_,{input})=>{
+            console.log(input)
+
+            return " creando ..."
+        }
     }
+
 }
 
 module.exports = resolvers
