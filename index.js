d674909977ce8198d71d92a3de0c3bc5781ac5bd
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-26 18:21:45 -0300

    Agrego types y resolvers de prueba

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
 const {ApolloServer, gql} = require("apollo-server")
 
+
+// Schema
+
+const typeDefs = gql`
+
+    type Curso {
+        titulo : String
+        tecnologia : String
+    }
+    type Query {
+        obtenerCursos : [Curso]
+    }
+`;
+
+const cursos = [
+    {
+        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+        tecnologia: 'JavaScript ES6',
+    },
+    {
+        titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+        tecnologia: 'React',
+    },
+    {
+        titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+        tecnologia: 'Node.js'
+    }, 
+    {
+        titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+        tecnologia: 'React'
+    }
+];
+
+
+//resolvers
+
+const resolvers ={
+    Query:{
+        obtenerCursos : ()=> cursos
+        
+    }
+}
+
+
 // Servidor 
 
-const server = new ApolloServer()
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
 
 
 // Arrancar el Servidor
