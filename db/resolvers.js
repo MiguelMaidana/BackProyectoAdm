564150243becd5af55f3d9c3b3752444f9c84748
Miguel Maidana <maidana.miguell@gmail.com>
2020-05-27 19:23:54 -0300

    Actualizar un Cliente. Eliminar Un cliente. Siempre respetando que sea validado por vendedor con context

--- db/resolvers.js
+++ db/resolvers.js
@@ -202,6 +202,48 @@ const resolvers ={
             }
 
             
+        },
+        actualizarCliente : async(_,{id,input},ctx) =>{
+
+            // verificar si existe o no 
+
+            let cliente = await Cliente.findById(id)
+            if(!cliente){
+                throw new Error ("El cliente no existe")
+            }
+
+            // verificar si el vendedor es quien edita
+            if(cliente.vendedor.toString() !== ctx.usuario.id){
+                throw new Error("No tienes las credenciales que corresponden para Editar ")
+            }
+
+            // guardar el cliente
+
+            cliente = await Cliente.findOneAndUpdate({_id:id},input, {new:true})
+            return cliente
+
+        },
+        eliminarCliente: async(_,{id},ctx)=>{
+             // verificar si existe o no 
+
+             let cliente = await Cliente.findById(id)
+             if(!cliente){
+                 throw new Error ("El cliente no existe")
+             }
+ 
+             // verificar si el vendedor es quien edita
+             if(cliente.vendedor.toString() !== ctx.usuario.id){
+                 throw new Error("No tienes las credenciales que corresponden para Editar ")
+             }
+
+             // Eliminar Cliente
+
+             await Cliente.findOneAndDelete({_id:id})
+
+             return "Cliente Eliminado"
+
+
+ 
         }
     }
 
